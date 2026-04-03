c855f7a0c20cc04f5d82e872e43d7662482f00f9
Alexis Horteales <ahortealese001@alumno.uaemex.mx>
2026-04-02 18:17:51 -0600

    Clean upload

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Alexis Horteales Espinosa on 02/04/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
